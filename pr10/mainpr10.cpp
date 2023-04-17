495decdcd93502215527d87e5a92d9809750470d
mannco1 <70008207+mannco1@users.noreply.github.com>
2023-04-17 23:30:44 +0300

    Add files via upload
    Signed-off-by: mannco1 <70008207+mannco1@users.noreply.github.com>

---
+++ pr10/mainpr10.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+#include "Header.h"
+using namespace std;
+
+
+
+int main()
+{
+
+    setlocale(LC_ALL, "Russian");
+    type5 equation1(2, -4);
+    equation1.Get_answer();
+    equation1.show();
+    
+    
+
+   
+    type6 equation2(1, -5, 6);
+    equation2.Get_answer();
+    equation2.show();
+    
+    
+
+    return 0;
+}
