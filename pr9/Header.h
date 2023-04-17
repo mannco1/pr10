ab085812b0549043e6e824e38e7fa830e67ea0d7
mannco1 <70008207+mannco1@users.noreply.github.com>
2023-04-17 12:24:30 +0300

    Add files via upload

---
+++ pr9/Header.h
@@ -0,0 +1,19 @@
+class type5 {
+private:
+    double B, C;
+public:
+    type5(double b1, double c1);
+    void Get_answer();
+    void show();
+};
+
+class type6 {
+private:
+    double A, B, C;
+public:
+    type6(double a1, double b1, double c1);
+        
+    void Get_answer();
+        
+    void show();
+};
